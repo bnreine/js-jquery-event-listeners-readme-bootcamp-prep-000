81d6fd82e9dabf8bd49b16f0eefc868466658dc1
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-24 17:53:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,33 +1,9 @@
-//alert('hey sarah');
-
-
-
 function  getIt() {
   $('p').addEventListener('click', function(event) {
     alert('"Hey"!');
    })
 }
 
-
-
-
-
-/*
-  it('getIt() binds a click event to the paragraph', function() {
-    window.alert = expect.createSpy()
-
-    window.getIt();
-
-    window.$('p')[0].click()
-
-    expect(window.alert).toHaveBeenCalled();
-  });
-
-
-+ Define a function `getIt` that does not accept a parameter. The function
-should bind a `click` event to the `p` tag. When the paragraph is clicked, the
-function should alert `"Hey!"`.
-*/
 
 
 $(document).ready(function(){
