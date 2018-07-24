7113a4337b2ff1d0c31edc1dff225205c813a9d4
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-24 18:09:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,22 +11,12 @@ function frameIt() {
   })
 }
 
+
+
 /*
-  it('frameIt() binds an event that frames the minion when the page loads', function() {
-    window.frameIt()
 
-    const img = window.$('img')
-
-    img.trigger('load');
-
-    expect(img.attr("class")).toEqual("tasty");
-  });
-
-
-+ Define a function `frameIt` that does not accept a parameter. The function
-should bind the `load` event that adds the class `tasty` to the image to add a
-red frame to the image.
 */
+
 
 $(document).ready(function(){
   getIt();
