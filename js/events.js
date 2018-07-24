2da8badb7016e5ca29cfa1b978409a5dab22c331
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-24 18:10:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,31 @@ function frameIt() {
 
 
 /*
+  describe('pressIt()', () => {
+    it('pressIt() binds an event that alerts the user when the G key was pressed down', function() {
+      window.alert = expect.createSpy()
 
+      window.pressIt();
+
+      const input = window.$('#typing');
+      const event = window.$.Event('keydown', { which: 71 })
+      input.trigger(event)
+
+      expect(window.alert).toHaveBeenCalled()
+    })
+
+    it('does not alert if another key is pressed', () => {
+      window.alert = expect.createSpy()
+
+      window.pressIt();
+
+      const input = window.$('#typing');
+      const event = window.$.Event('keydown', { which: 70 })
+      input.trigger(event)
+
+      expect(window.alert).toNotHaveBeenCalled()
+    })
+  })
 
 + Define a function `pressIt` that does not accept a parameter. The function
 should bind a `keydown` event to the input field of the form that alerts a user
