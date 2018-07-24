3b621ac45c9c8eafbab1829440e3bf45c8d99291
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-24 18:08:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,14 +7,9 @@ function  getIt() {
 */
 
 function frameIt() {
-  //console.log('yo my man');
-  return $('img');
-  /*
   $('img').addEventListener('load', function(event) {
     $('img').setAttribute('class', 'tasty');
-  }  )
-  */
-  
+  })
 }
 
 /*
