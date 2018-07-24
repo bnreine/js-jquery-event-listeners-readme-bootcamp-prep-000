837b5f426c9ee57743e57bc8edca61ad70d271a1
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-24 17:56:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,7 +5,14 @@ function  getIt() {
 }
 
 
+/*
 
+
+
++ Define a function `frameIt` that does not accept a parameter. The function
+should bind the `load` event that adds the class `tasty` to the image to add a
+red frame to the image.
+*/
 
 $(document).ready(function(){
   getIt();
